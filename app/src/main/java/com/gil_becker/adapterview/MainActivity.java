17c1806e3ab9346027fb3f467bb3007e53b2b0d8
Gil Becker <becker.gil@gmail.com>
2017-03-29 17:26:51 +0300

    1111

--- app/src/main/java/com/gil_becker/adapterview/MainActivity.java
+++ app/src/main/java/com/gil_becker/adapterview/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        System.out.println("gil");
+
         setContentView(R.layout.activity_main);
         displayGrid();
     }
